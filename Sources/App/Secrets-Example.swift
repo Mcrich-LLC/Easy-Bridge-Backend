9961900d4f6a8ca69aad3792b4957b4c676e4f3c
Morris Richman <mcrich23@icloud.com>
2022-08-19 08:30:06 -0700

    added another module to username and password

--- Sources/App/Secrets-Example.swift
+++ Sources/App/Secrets-Example.swift
@@ -12,4 +12,6 @@ import Foundation
 enum SecretsExample {
     static let twitterbearerToken = "paste-your-twitter-api-key-here-and-replace-this-string--example: aaaaaaapapppppiiiiiiiiiKeeeeeeeeyyyyyyyyy"
     static let editBearerToken = "internal-bearer-token-to-update-database-*you*-come-up-with-this"
+    static let username: String? = nil
+    static let password: String? = nil
 }
