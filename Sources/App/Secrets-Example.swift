dad6485f791d5903cb30106f6cebebd73884b647
Morris Richman <mcrich23@icloud.com>
2022-08-19 07:40:58 -0700

    Create Secrets-Example.swift

---
+++ Sources/App/Secrets-Example.swift
@@ -0,0 +1,15 @@
+//
+//  Secrets-Example.swift
+//  
+//
+//  Created by Morris Richman on 8/19/22.
+//
+
+import Foundation
+
+// swiftlint:disable line_length
+
+enum SecretsExample {
+    static let twitterbearerToken = "paste-your-twitter-api-key-here-and-replace-this-string--example: aaaaaaapapppppiiiiiiiiiKeeeeeeeeyyyyyyyyy"
+    static let editBearerToken = "internal-bearer-token-to-update-database-*you*-come-up-with-this"
+}
