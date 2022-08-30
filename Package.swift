1594b6dd7a380fcdf4c917eba91ef1fd96c1d209
Morris Richman <mcrich23@icloud.com>
2022-08-29 18:53:24 -0700

    Added Alamofire to Package.swift

--- Package.swift
+++ Package.swift
@@ -11,7 +11,8 @@ let package = Package(
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
         .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
         .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.0.0"),
-        .package(url: "https://github.com/MihaelIsaev/VaporCron", from: "2.6.0")
+        .package(url: "https://github.com/MihaelIsaev/VaporCron", from: "2.6.0"),
+        .package(url: "https://github.com/Alamofire/Alamofire", from: "5.6.1")
     ],
     targets: [
         .target(
@@ -20,7 +21,8 @@ let package = Package(
                 .product(name: "Fluent", package: "fluent"),
                 .product(name: "FluentPostgresDriver", package: "fluent-postgres-driver"),
                 .product(name: "Vapor", package: "vapor"),
-                .product(name: "VaporCron", package: "VaporCron")
+                .product(name: "VaporCron", package: "VaporCron"),
+                .product(name: "Alamofire", package: "Alamofire")
             ],
             swiftSettings: [
                 // Enable better optimizations when building in Release configuration. Despite the use of
