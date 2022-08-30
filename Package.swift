94489c0d56baad526467ab2b576eeb2948cf417e
Morris Richman <mcrich23@icloud.com>
2022-08-29 20:16:43 -0700

    removed Alamofire

--- Package.swift
+++ Package.swift
@@ -11,8 +11,7 @@ let package = Package(
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
         .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
         .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.0.0"),
-        .package(url: "https://github.com/MihaelIsaev/VaporCron", from: "2.6.0"),
-        .package(url: "https://github.com/Alamofire/Alamofire", from: "5.6.1")
+        .package(url: "https://github.com/MihaelIsaev/VaporCron", from: "2.6.0")
     ],
     targets: [
         .target(
@@ -21,8 +20,7 @@ let package = Package(
                 .product(name: "Fluent", package: "fluent"),
                 .product(name: "FluentPostgresDriver", package: "fluent-postgres-driver"),
                 .product(name: "Vapor", package: "vapor"),
-                .product(name: "VaporCron", package: "VaporCron"),
-                .product(name: "Alamofire", package: "Alamofire")
+                .product(name: "VaporCron", package: "VaporCron")
             ],
             swiftSettings: [
                 // Enable better optimizations when building in Release configuration. Despite the use of
--- Sources/App/Twitter Models/Fetch Data.swift
+++ Sources/App/Twitter Models/Fetch Data.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import Alamofire
 #if canImport(FoundationNetworking)
 import FoundationNetworking
 #endif
@@ -15,87 +16,14 @@ enum HttpError: Error {
     case badURL
 }
 
-class TwitterFetch : NSObject, URLSessionDataDelegate {
+class TwitterFetch {
     
     static var shared = TwitterFetch()
     
-    private var session: URLSession! = nil
-    
-    override init() {
-        super.init()
-        let config = URLSessionConfiguration.default
-        config.requestCachePolicy = .reloadIgnoringLocalCacheData
-        self.session = URLSession(configuration: config, delegate: self, delegateQueue: .main)
-    }
-    
-    private var streamingTask: URLSessionDataTask? = nil
-    
-    var isStreaming: Bool { return self.streamingTask != nil }
-    
-    func startStreaming() {
-        print("start stream")
-        precondition( !self.isStreaming )
+    func startStream() {
         
-        let url = URL(string: "https://api.twitter.com/2/tweets/search/stream?tweet.fields=id,text")!
-        var request = URLRequest(url: url)
-        request.addValue("Bearer \(Secrets.twitterBearerToken)", forHTTPHeaderField: "Authorization")
-        let task = self.session.uploadTask(withStreamedRequest: request)
-        self.streamingTask = task
-        task.resume()
     }
     
-    func stopStreaming() {
-        guard let task = self.streamingTask else {
-            return
-        }
-        self.streamingTask = nil
-        task.cancel()
-        self.closeStream()
-    }
-    
-    var outputStream: OutputStream? = nil
-    
-    private func closeStream() {
-        if let stream = self.outputStream {
-            stream.close()
-            self.outputStream = nil
-        }
-    }
-    
-    func urlSession(_ session: URLSession, task: URLSessionTask, needNewBodyStream completionHandler: @escaping (InputStream?) -> Void) {
-        self.closeStream()
-        
-        var inStream: InputStream? = nil
-        var outStream: OutputStream? = nil
-        Stream.getBoundStreams(withBufferSize: 4096, inputStream: &inStream, outputStream: &outStream)
-        self.outputStream = outStream
-        
-        completionHandler(inStream)
-    }
-    
-    func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
-        NSLog("task data: %@", data as NSData)
-        
-        do {
-            let jsonDecoder = JSONDecoder()
-            jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
-            let result = try jsonDecoder.decode([Tweet].self, from: data)
-            print("result = \(result)")
-            for tweet in result {
-                BridgeFetch.handleBridge(text: tweet.text)
-            }
-        } catch {
-            print("error, could not decode")
-        }
-    }
-    
-    func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
-        if let error = error as NSError? {
-            NSLog("task error: %@ / %d", error.domain, error.code)
-        } else {
-            NSLog("task complete")
-        }
-    }
     func fetchTweet(id: String, completion: @escaping (Result<Response, Error>) -> Void) {
             do {
                 print("started twitter fetch")
