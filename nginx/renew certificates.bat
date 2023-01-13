40cf68956ffeda6bce4f57f6522935b205287216
Mcrich23 <81453549+Mcrich23@users.noreply.github.com>
2023-01-12 18:30:07 -0800

    Fixed renew certificates

--- nginx/renew certificates.bat
+++ nginx/renew certificates.bat
@@ -1,4 +1,9 @@
 @echo off
 
+cd /D "%~dp0"
 certbot renew --dry-run
+copy /Y C:\Certbot\live\mc.mcrich23.com\privkey.pem key.pem
+copy /Y C:\Certbot\live\mc.mcrich23.com\fullchain.pem fullchain.pem
+copy /Y C:\Certbot\live\mc.mcrich23.com\chain.pem chain.pem
+copy /Y C:\Certbot\live\mc.mcrich23.com\cert.pem cert.pem
 pause
