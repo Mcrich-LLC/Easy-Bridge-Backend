51634117723136f98f0a4cd3f39ef865fa080265
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2023-04-30 13:21:58 -0700

    Update Bridge Fetch.swift

--- Sources/App/Bridge Fetch.swift
+++ Sources/App/Bridge Fetch.swift
@@ -43,7 +43,7 @@ struct BridgeFetch {
               "to": "/topics/subscription_check",
               "mutable_content": true,
               "notification": {
-                "title": "subscription check",
+                "title": "",
                 "body": "",
                 "badge": 0,
                 "sound": "default",
