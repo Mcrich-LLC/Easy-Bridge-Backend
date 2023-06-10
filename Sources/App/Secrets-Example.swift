1c279111a805b3db746e1bd4295155f6e4608632
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2023-06-10 12:19:40 -0700

    added firebaseWebAuthToken to secrets example

--- Sources/App/Secrets-Example.swift
+++ Sources/App/Secrets-Example.swift
@@ -13,6 +13,7 @@ enum SecretsExample {
     static let twitterbearerToken = "paste-your-twitter-api-key-here-and-replace-this-string--example: aaaaaaapapppppiiiiiiiiiKeeeeeeeeyyyyyyyyy"
     static let internalEditBearerToken = "internal-bearer-token-to-update-database-*you*-come-up-with-this"
     static let firebaseCloudMessagingBearerToken = "internal-bearer-token-to-send-notifications"
+    static let firebaseWebAuthToken = "firebase-web-token"
     
     static func authorizeToken(token: String?) -> Bool {
         if token == internalEditBearerToken {
