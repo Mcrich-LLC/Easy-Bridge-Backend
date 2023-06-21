2333a0b2543c8b828135b3146bb97db27d9d9cb1
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2023-06-21 01:31:46 -0700

    Update Bridge Fetch.swift

--- Sources/App/Bridge Fetch.swift
+++ Sources/App/Bridge Fetch.swift
@@ -113,7 +113,8 @@ struct BridgeFetch {
                 FcmManager.shared.send(pref.deviceId.stringValue, title: bridgeDetails.bridgeLocation, body: "The \(bridge.name.capitalized) is now \(status)", data: [
                     "badge": "0",
                     "sound": "default",
-                    "content_availible": "true",
+                    "mutable_content": "true",
+                    "priority": "high",
                     "interruption_level": "\(pref.notificationPriorityAsInt())",
                     "bridge_id": "\(bridgeDetails.id)"
                 ])
