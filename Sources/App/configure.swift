48b84a56069eeb9409a5d55754020d06e6766e46
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2023-07-31 15:27:08 -0700

    Update configure.swift

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -12,7 +12,7 @@ public func configure(_ app: Application) async throws {
 
     app.databases.use(.postgres(configuration: SQLPostgresConfiguration(
         hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
+        port: Environment.get("APP_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
         username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
         password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
         database: Environment.get("DATABASE_NAME") ?? "vapor_database",
