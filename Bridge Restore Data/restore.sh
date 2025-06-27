60adf58a118237a9d67715e6514ee1aaed731733
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2025-06-26 20:51:26 -0700

    Update restore.sh

--- Bridge Restore Data/restore.sh
+++ Bridge Restore Data/restore.sh
@@ -3,11 +3,18 @@
 # Get the directory of the script
 script_dir=$(dirname "$0")
 
+# Determine whether to use sudo for docker commands
+if [ "$EUID" -eq 0 ]; then
+    docker_cmd="sudo docker"
+else
+    docker_cmd="docker"
+fi
+
 # Prompt the user to enter the Docker container name
 read -p "Enter your Docker container name: " your_db_container
 
 # Check if the container exists
-sudo docker inspect $your_db_container > /dev/null 2>&1
+$docker_cmd inspect $your_db_container > /dev/null 2>&1
 if [ $? -ne 0 ]; then
     echo "Error: Docker container '$your_db_container' not found."
     exit 1
@@ -28,5 +35,5 @@ if [ ! -f "$your_dump_file" ]; then
     exit 1
 fi
 
-# Command to execute
-cat "$your_dump_file" | sudo docker exec -i $your_db_container psql -U $your_db_user -d $your_db_name
+# Execute the SQL file inside the container
+cat "$your_dump_file" | $docker_cmd exec -i "$your_db_container" psql -U "$your_db_user" -d "$your_db_name"
